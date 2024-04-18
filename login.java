231f7d02e14eef4048436559b0743a95f1ee76bb
Pranav Ausekar <pranavausekar1@gmail.om>
2024-04-18 18:54:20 +0530

    files added

---
+++ login.java
@@ -0,0 +1,14 @@
+
+import javax.swing.*;
+
+public class login extends JFrame {
+    login() {
+        super("Bank Management System");
+        setSize(850, 480);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        login l = new login();
+    }
+}
