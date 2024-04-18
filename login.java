b619bf70b42ea810f33cf72a6810a0bb7643904e
pritinavale27 <pritinavale365@gmail.com>
2024-04-18 19:11:37 +0530

    Update login.java
    added bank image

--- login.java
+++ login.java
@@ -1,10 +1,29 @@
 
+import java.awt.Image;
+
 import javax.swing.*;
 
 public class login extends JFrame {
     login() {
         super("Bank Management System");
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Icons/bank.png"));
+        Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3); 
+        image.setBounds(350, 10, 100,  100);
+        add(image);
+
+    //    ImageIcon ii1 = new ImageIcon(ClassLoader.getSystemResource("Icons/bank.png"));
+    //     Image ii2 = ii1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+    //     ImageIcon ii3 = new ImageIcon(ii2);
+    //     JLabel image = new JLabel(ii3); 
+    //     image.setBounds(350, 10, 100,  100);
+    //     add(image);  
+
+        setLayout(null);
         setSize(850, 480);
+        setLocation(450, 200);
         setVisible(true);
     }
 
